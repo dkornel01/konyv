ad62f23404e0810a47befb4276beffd71fae8514
Keresztes Hunor (SZF_2022_2024) <keresztes.hunor@diak.szamalk-szalezi.hu>
2023-12-06 13:05:38 +0100

    :cat2::dash:

--- src/konyvtarprogram/Konyvtar.java
+++ src/konyvtarprogram/Konyvtar.java
@@ -1,6 +1,49 @@
 package konyvtarprogram;
 
+import java.util.UUID;
+
 public class Konyvtar
 {
+    private final AbstractTermek[] termekek;
     
+    public Konyvtar(int dbTermek)
+    {
+        termekek = new AbstractTermek[dbTermek];
+    }
+    
+    public void felvesz(AbstractTermek termek)
+    {
+        int i = 0;
+        while (i < termekek.length && termekek[i] != null)
+        {
+            i++;
+        }
+        if (i < termekek.length)
+        {
+            termekek[i] = termek;
+        }
+        else
+        {
+            System.out.println("Tele van a könyvtár!");
+        }
+    }
+    
+    public void termekekKiir()
+    {
+        for (AbstractTermek termek : termekek)
+        {
+            System.out.println(UUID.randomUUID() + ": " + termek);
+        }
+    }
+    
+    public void kolcsonozhetoketKiir()
+    {
+        for (AbstractTermek termek : termekek)
+        {
+            if (termek instanceof Kolcsonozheto k)
+            {
+                System.out.println(UUID.randomUUID() + ": " + k);
+            }
+        }
+    }
 }
--- src/konyvtarprogram/KonyvtarProgram.java
+++ src/konyvtarprogram/KonyvtarProgram.java
@@ -2,8 +2,22 @@ package konyvtarprogram;
 
 public class KonyvtarProgram
 {
+    private final Konyvtar konyvtar;
+    
     public static void main(String[] args)
     {
-        
+        new KonyvtarProgram().run();
+    }
+    
+    private KonyvtarProgram()
+    {
+        konyvtar = new Konyvtar(5);
+    }
+    
+    private void run()
+    {
+        // itt fel kell vinni egy pár terméket
+        konyvtar.termekekKiir();
+        konyvtar.kolcsonozhetoketKiir();
     }
 }
